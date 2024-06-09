880717f07ba5539db3f1800a48fed48ac075f6a9
leandrodelimac <leandrolima7472@gmail.com>
2024-06-09 15:49:05 -0300

    route navigation, loading microfrontends using footer and header cache

--- packages/app-header/src/root.component.tsx
+++ packages/app-header/src/root.component.tsx
@@ -1,3 +1,14 @@
+// @ts-ignore
+import { Navigate } from "@shoppe/util-ui";
+
 export default function Root(props) {
-  return <section>Eu sou o HEADER!!!!!!</section>;
+  return (
+    <section>
+      <h1>Eu sou o HEADER!!!!!!</h1>
+      <Navigate path="/account">Account</Navigate>
+      <Navigate path="/cart">Cart</Navigate>
+      <Navigate path="/product">Product</Navigate>
+      <Navigate path="/shop">Shop</Navigate>
+    </section>
+  );
 }
--- packages/util-ui/src/shoppe-util-ui.tsx
+++ packages/util-ui/src/shoppe-util-ui.tsx
@@ -1,2 +1,18 @@
-// Anything exported from this file is importable by other in-browser modules.
-export function publicApiFunction() {}
+import { navigateToUrl } from "single-spa";
+
+interface INavigateProps {
+  path: string;
+  children?: React.ReactNode;
+}
+
+export const Navigate: React.FC<INavigateProps> = (props) => {
+  function redirect(e: React.MouseEvent<HTMLButtonElement>) {
+    navigateToUrl(e.currentTarget.name);
+  }
+
+  return (
+    <button name={props.path} onClick={redirect}>
+      {props.children}
+    </button>
+  );
+};
